8fa168ee627728826a42082d8f8d66554d6e7f99
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2023-03-14 02:49:57 +0530

    Error Handling

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -3,6 +3,12 @@ const ErroHandler = require("../utils/errorhandler");
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Error";
+  //Wrong MongoDb Id Error
+  if (err.name === "CastError") {
+    const message = `Resource not found Invalid:${err.path}`;
+    err = new ErroHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: true,
     message: err.message,
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
-// const {authenticate} = require('./../middleware/authenticate');
+
+// handling UnCaught Expection
+process.on("uncaughtException", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log("Shutting Down the Server due to UnCaught Expection ");
+  process.exit(1);
+});
+
 // config
 dotenv.config({ path: "backend/config/config.env" });
 
 // connecting to database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server is Working on  http://localhost:${process.env.PORT}`);
+});
+
+//unhandlePromise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log("Shutting Down the Server due to unhandle promise Rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
